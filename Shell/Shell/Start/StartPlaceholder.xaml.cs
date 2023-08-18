d851cab618d1bf5f3a885b891f9b55f41d7cf996
Lixkote <95425619+Lixkote@users.noreply.github.com>
2023-08-18 10:18:56 +0200

    Make the left chrome buttons functional
    Make the left chrome buttons functional  + working user avatar preview.

--- Shell/Shell/Start/StartPlaceholder.xaml.cs
+++ Shell/Shell/Start/StartPlaceholder.xaml.cs
@@ -1,5 +1,13 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.ApplicationModel.Contacts;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.System;
+using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Media.Imaging;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -15,68 +23,77 @@ namespace ShellApp.Shell.Start
         public StartPlaceholder()
         {
             this.InitializeComponent();
-            CreatePowerOptionsMenuFlyout();
+            this.Loaded += Start_Loaded;
         }
 
-
-        public void CreatePowerOptionsMenuFlyout()
+        private async void Start_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
-            // Create a new MenuFlyoutItem with an icon for each available power state
+            IReadOnlyList<User> users = await User.FindAllAsync();
 
-            FontIcon sleep = new FontIcon();
-            sleep.Glyph = "\uE708";
+            var current = users.Where(p => p.AuthenticationStatus == UserAuthenticationStatus.LocallyAuthenticated &&
+                                        p.Type == UserType.LocalUser).FirstOrDefault();
 
-            FontIcon hibernate = new FontIcon();
-            hibernate.Glyph = "\uE823";
-
-            FontIcon power = new FontIcon();
-            power.Glyph = "\uE7E8";
-
-            FontIcon restart = new FontIcon();
-            restart.Glyph = "\uE72C";
-
-            var sleepItem = new MenuFlyoutItem
+            // user may have picture
+            var picture = await current.GetPictureAsync(UserPictureSize.Size64x64);
+            if (picture != null)
             {
-                Text = "Sleep",
-                Icon = sleep,
-                Tag = "PowerState.Sleep"
-            };
-            PowerFlyout.Items.Add(sleepItem);
+                var imageStream = await picture.OpenReadAsync();
+                // Create a BitmapImage object
+                BitmapImage bitmapImage = new BitmapImage();
 
-            var hibernateItem = new MenuFlyoutItem
-            {
-                Text = "Hibernate",
-                Icon = hibernate,
-                Tag = "PowerState.Hibernate"
-            };
+                // Set the source of the BitmapImage from the stream
+                await bitmapImage.SetSourceAsync(imageStream);
+                UserAV.Source = bitmapImage;
+            }
 
-            PowerFlyout.Items.Add(hibernateItem);
+        }
 
-            var powerItem = new MenuFlyoutItem
-            {
-                Text = "Power",
-                Icon = power,
-                Tag = "PowerState.Shutdown"
-            };
-            PowerFlyout.Items.Add(powerItem);
+        private void PowerButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
 
-            var restartItem = new MenuFlyoutItem
-            {
-                Text = "Restart",
-                Icon = restart,
-                Tag = "PowerState.Restart"
-            };
-            PowerFlyout.Items.Add(restartItem);
+        }
 
+        private async void SettingsButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            await Windows.System.Launcher.LaunchUriAsync(new Uri("ms-settings:"));
+        }
 
+        private async void ImagesButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            // Get the user's pictures folder
+            StorageFolder picturesFolder = KnownFolders.PicturesLibrary;
 
+            // Launch the folder in the default file explorer app
+            await Launcher.LaunchFolderAsync(picturesFolder);
+        }
 
+        private async void DocumentsButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            // Get the user's pictures folder
+            StorageFolder docsFolder = KnownFolders.DocumentsLibrary;
 
+            // Launch the folder in the default file explorer app
+            await Launcher.LaunchFolderAsync(docsFolder);
+        }
 
+        private void HibernateMenuButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            //Nothing here as i need to bind this action from WPF part of the app
+        }
 
+        private void SleepMenuButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            //Nothing here as i need to bind this action from WPF part of the app
+        }
 
+        private void PowerMenuButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            ShutdownManager.BeginShutdown(ShutdownKind.Shutdown, TimeSpan.Zero);
+        }
 
-
+        private void RestartMenuButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            ShutdownManager.BeginShutdown(ShutdownKind.Restart, TimeSpan.Zero);
         }
     }
 }
