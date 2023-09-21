f8381f7a20e1fc7475d43c609e118a9096c72eec
Lixkote <95425619+Lixkote@users.noreply.github.com>
2023-09-21 08:00:33 +0200

    Collapsable Child folder items
    Collapsable Child folder items

---
+++ Shell/Shell/ShellUtils/StartEntrySelector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml;
+using System.Collections.ObjectModel;
+
+namespace Shell.Shell.ShellUtils
+{
+    public class StartMenuSelector : DataTemplateSelector
+    {
+        public DataTemplate AppD { get; set; }
+        public DataTemplate Folder { get; set; }
+        protected override DataTemplate SelectTemplateCore(object item)
+        {
+            if (item.ToString() == "WPF.Helpers.StartMenuLink")
+            {
+                return AppD;
+            }
+            else if (item.ToString() == "WPF.Helpers.StartMenuDirectory")
+            {
+                return Folder;
+            }
+            else
+            {
+                return base.SelectTemplateCore(item);
+            }
+        }
+    }
+}
--- Shell/Shell/Start/StartPlaceholder.xaml.cs
+++ Shell/Shell/Start/StartPlaceholder.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shell.Shell.ShellUtils;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
